e8f9979a356da5e43235e5484824a45bd4ba165b
alfie <alfie.2012@gmail.com>
2014-10-12 00:51:23 +0530

    Setup basic pages

--- calc/views.py
+++ calc/views.py
@@ -1,6 +1,15 @@
 from calc import app
+from flask import render_template
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return 'This is the Index Page'
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
